91f236d6afa01156af406a8b7411c8841df529df
SOUP <soupyong@gmail.com>
2019-06-29 12:13:25 +0900

    App: Use 'zxing-android-embedded' again, 'cause of rendering issue

--- app/src/main/java/soup/qr/core/encoder/BarcodeImage.kt
+++ app/src/main/java/soup/qr/core/encoder/BarcodeImage.kt
@@ -2,6 +2,7 @@ package soup.qr.core.encoder
 
 import android.graphics.Bitmap
 import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
 
 class BarcodeImage(private val barcodeFormat: BarcodeFormat) {
 
